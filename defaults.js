a31158b1da63709acb74f7a8d3ca4297245abd4e
Diego Chavão <diegochavao+git@proton.me>
2026-05-01 11:14:46 -0300

    feat: add default blocked patterns

---
+++ defaults.js
@@ -0,0 +1,11 @@
+window.BLOCK_SITES_STORAGE_KEY = "blockedUrlPatterns";
+
+window.BLOCK_SITES_DEFAULTS = Object.freeze([
+  "https://x.com/*",
+  "https://www.youtube.com/*",
+  "https://mail.google.com/*",
+  "https://www.instagram.com/*",
+  "https://www.reddit.com/*",
+  "https://www.facebook.com/*",
+  "https://web.whatsapp.com/*"
+]);
